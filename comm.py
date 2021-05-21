6ce4c360184550630138cea95ae3c0db9d301298
xiexiaoxuan <15735185585@163.com>
2021-05-21 09:47:23 +0800

    update code

--- comm.py
+++ comm.py
@@ -25,7 +25,7 @@ TEST_FILE = os.path.join(DATASET_PATH, "test_a.csv")
 END_DAY = 15
 
 # 随机
-SEED = 2021
+SEED = 666
 
 # 初赛待预测行为列表
 ACTION_LIST = ["read_comment", "like", "click_avatar",  "forward"]
@@ -38,7 +38,7 @@ ACTION_SAMPLE_RATE = {"read_comment": 5, "like": 5, "click_avatar": 5, "forward"
 # 各个阶段数据集的设置的最后一天
 STAGE_END_DAY = {"online_train": 14, "offline_train": 12, "evaluate": 13, "submit": 15}
 # 各个行为构造训练数据的天数
-ACTION_DAY_NUM = {"read_comment": 5, "like": 5, "click_avatar": 5, "forward": 5, "comment": 5, "follow": 5, "favorite": 5}
+ACTION_DAY_NUM = {"read_comment": 10, "like": 10, "click_avatar": 10, "forward": 10, "comment": 10, "follow": 10, "favorite": 10}
 
 
 def create_dir():
